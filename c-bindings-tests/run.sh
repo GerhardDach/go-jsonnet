01ce61cc04be28d9f50a1b718269cb7b9938fb46
GerhardDach <wjdmhoztuk@outlook.com>
2024-01-30 02:14:30 +0800

    sed

--- c-bindings-tests/run.sh
+++ c-bindings-tests/run.sh
@@ -1,4 +1,4 @@
-#!/bin/bash
+##/bin/bash
 set -e
 
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
@@ -7,3 +7,4 @@ cd "$DIR"
 ./build.sh
 echo "Running tests for C bindings"
 ./compat_test.py
+ 
